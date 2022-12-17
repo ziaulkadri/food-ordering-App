9c509bf4cc352a7e9f4ceb4411192f184c00b906
ziaulkadri <ziaul.kadri@innoage.in>
2022-12-17 18:55:55 +0530

    Add components

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,30 @@
+import { Fragment } from 'react';
+
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+	return (
+		<Fragment>
+			{' '}
+			{/*  used fragmenet as a wraper beacuse we need one
+			root element */}
+			<header className={classes.header}>
+				<h1>ReactMeals</h1>
+				<HeaderCartButton />
+			</header>
+			<div className={classes['main-image']}>
+				{' '}
+				{/*  we use classname like that beacuse
+				the classname has dash inside its name */}
+				<img
+					src={mealsImage}
+					alt="A table full of delicious food!"
+				/>
+			</div>
+		</Fragment>
+	);
+};
+
+export default Header;
